44d54c7caa9ff5ddd7dbe406e32fb1a0c77374f2
kellya73 <32706271+kellya73@users.noreply.github.com>
2017-11-01 13:26:15 +0000

    Add files via upload
    Commit1

---
+++ Edge.java
@@ -0,0 +1,17 @@
+package directedAcyclicGraph;
+
+public class Edge {
+
+	
+	public int a;
+	public int b;
+	public int c;
+	
+	public Edge(int a, int b, int c)
+	{
+		this.a=a;
+		this.b=b;
+		this.c=c;
+	}
+	
+}
--- Graph.java
+++ Graph.java
@@ -1,5 +1,24 @@
 package directedAcyclicGraph;
+import java.util.ArrayList;
 
 public class Graph {
+	public static ArrayList<Edge>[] adjacencyList=null;
+	public static int[] inEdgeCount=null;
+	public int numberOfVertices;
+
+	public Graph(int numberOfVertices)
+	{
+		adjacencyList=(ArrayList<Edge>[])new ArrayList[numberOfVertices+1];
+		
+		for(int i=1 ; i<=numberOfVertices ; i++)
+		{
+			adjacencyList[i]=new ArrayList<Edge>();
+		}
+		
+		inEdgeCount=new int[numberOfVertices+1];
+		this.numberOfVertices=numberOfVertices;
+	}
+	
+	
 
 }
