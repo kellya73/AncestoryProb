2c349630ab52ba1cdd4df8f0d8549cefcb9a08ef
kellya73 <32706271+kellya73@users.noreply.github.com>
2017-11-01 17:43:56 +0000

    commit2

--- Graph.java
+++ Graph.java
@@ -2,22 +2,68 @@ package directedAcyclicGraph;
 import java.util.ArrayList;
 
 public class Graph {
-	public static ArrayList<Edge>[] adjacencyList=null;
+	
+	public static ArrayList<Edge>[] adjList=null;
 	public static int[] inEdgeCount=null;
 	public int numberOfVertices;
 
 	public Graph(int numberOfVertices)
 	{
-		adjacencyList=(ArrayList<Edge>[])new ArrayList[numberOfVertices+1];
+		adjList=(ArrayList<Edge>[])new ArrayList[numberOfVertices+1];
 		
 		for(int i=1 ; i<=numberOfVertices ; i++)
 		{
-			adjacencyList[i]=new ArrayList<Edge>();
+			adjList[i]=new ArrayList<Edge>();
 		}
-		
+
 		inEdgeCount=new int[numberOfVertices+1];
-		this.numberOfVertices=numberOfVertices;
+		//this.numberOfVertices=numberOfVertices;
 	}
+
+	public void createEdge(int a, int b, int c)
+	{
+		if(adjList[a]==null)
+		{
+			adjList[a]=new ArrayList<Edge>();
+			adjList[a].add(new Edge(a, b, c));
+		}
+	}
+	
+	public void removeEdge(int a, int b)
+	{
+		int indexToBeRemoved=-1;
+		ArrayList<Edge> edgeList=adjList[a];
+		for(int i=0;i<adjList[a].size();i++)
+		{
+			Edge e=edgeList.get(i);
+			if(e.a==a && e.b==b)
+			{
+				indexToBeRemoved=i;
+			}
+		}
+		if(indexToBeRemoved!=-1)
+			edgeList.remove(indexToBeRemoved);
+	}
+	
+	
+	public int[] EdgesCount()
+	{
+		for(int i=1;i<=numberOfVertices;i++)
+		{
+			for(int j=1;j<adjList.length;j++)
+			{
+				ArrayList<Edge> list=adjList[j];
+				for(Edge e : list)
+				{
+					if(e.b==i)
+						inEdgeCount[i]++;
+				}
+			}
+
+		}
+		return inEdgeCount;
+	}
+	
 	
 	
 
--- GraphTest.java
+++ GraphTest.java
@@ -2,13 +2,33 @@ package directedAcyclicGraph;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
 
 public class GraphTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testcreateEdge() {
+		//ArrayList<Edge> edgeList;
+		Graph graph=new Graph(3);
+		graph.createEdge(1, 2, 5);
+		graph.createEdge(1, 3, 12);
+		graph.createEdge(3, 1, 12);
+		graph.createEdge(2, 1, 5);
+		graph.createEdge(2, 3, 7);
+		graph.createEdge(3, 2, 7);
+
 	}
 
 }
